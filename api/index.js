b5c0e4ed7661557b392ad91cc222e0a7ea744a39
JCPrime01 <vanzokiller@gmail.com>
2026-06-10 14:16:28 -0300

    chore: shareCode 9G28HRPSEUJ -> 36D3MIVR1OF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=9G28HRPSEUJ&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zefg29pw?shareCode=36D3MIVR1OF&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
